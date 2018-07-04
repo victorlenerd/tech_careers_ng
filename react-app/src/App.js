5b5e11d14c987db86b08b6714c84adbe9aa13873
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-07-04 14:42:26 +0100

    updated pages with the navbar and header

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 import Home from './pages/home';
+import About from './pages/about';
+import Apply from './pages/apply';
+import Jobs from './pages/jobs';
+import Post from './pages/post';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <div>
+        <Post />
+      </div>
     );
   }
 }
---
+++ react-app/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavTop from '../components/navTop';
+import Header from '../components/header';
+
+export default () => (
+  <div>
+    <Header />
+    <NavTop />
+  </div>
+);
