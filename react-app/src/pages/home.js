5e029dad8498175641450e284ebde5dc40157f9d
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-07-05 12:50:33 +0100

    added the search area to the home page

--- react-app/src/pages/home.js
+++ react-app/src/pages/home.js
@@ -2,36 +2,50 @@ import React from 'react';
 import Header from '../components/header';
 
 export default () => (
-    <section className="main">
-        <Header />
-        <div className="jumbotron">
-            <div className="overlay"></div>
-            <div className="container">
-                <h1>Discover The Best Jobs<br />Oppurtunities In Tech.</h1>
-                <br />
-                <br />
-                <p>Tech careers is  platform for employers<br />and job seekers to find the <br />perfect match.</p>
-            </div>
+  <section className="main">
+    <Header />
+    <div className="jumbotron">
+      <div className="overlay" />
+      <div className="container">
+        <h1>
+          Discover The Best Jobs<br />Oppurtunities In Tech.
+        </h1>
+        <br />
+        <br />
+        <p>
+          Tech careers is platform for employers<br />and job seekers to find
+          the <br />perfect match.
+        </p>
+      </div>
+    </div>
+    <div className="job-search mx-auto p-2">
+      <div className="row mx-auto pl-4">
+        <div className="col-sm-9 py-2">
+          <div className="row">
+            <select className="col-sm-3 select-style">
+              <option>Here is the first option</option>
+              <option>The second option</option>
+              <option>The third option</option>
+            </select>
+            <select className="col-sm-3 select-style">
+              <option>Here is the first option</option>
+              <option>The second option</option>
+              <option>The third option</option>
+            </select>
+            <select className="col-sm-3 select-style">
+              <option>Here is the first option</option>
+              <option>The second option</option>
+              <option>The third option</option>
+            </select>
+            <select className="col-sm-3 select-style">
+              <option>Here is the first option</option>
+              <option>The second option</option>
+              <option>The third option</option>
+            </select>
+          </div>
         </div>
-        <div className="container">
-            <div className="job-search row">
-                <div className="col-lg-2 col-md-2 col-md-6 col-xs-12">
-                    <input type="text" className="control" placeholder="Type: e.g., fulltime..." aria-label="Type" aria-describedby="basic-addon1" />
-                </div>
-                <div className="col-lg-2 col-md-2 col-md-6 col-xs-12">
-                    <input type="text" className="control" placeholder="Role: e.g., backend..." aria-label="Role" aria-describedby="basic-addon1" />
-                </div>
-                <div className="col-lg-2 col-md-2 col-md-6 col-xs-12">
-                    <input type="text" className="control" placeholder="Location: e.g., Lagos..." aria-label="Location" aria-describedby="basic-addon1" />
-                </div>
-                <div className="col-lg-2 col-md-2 col-md-6 col-xs-12">
-                    <select placeholder="Experience: e.g., senior...">
-                    </select>
-                </div>
-                <div className="col-lg-2 col-md-2">
-                    <button type="submit" className="btn search-btn">SEARCH</button>
-                </div>
-            </div>
-        </div>
-    </section>
+        <button className="col-sm-2  offset-sm-1 btn btn-block">Search</button>
+      </div>
+    </div>
+  </section>
 );
