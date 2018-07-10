05781c2fdc3cbfba2a4ee683aff8d61b28f03d93
temiolakutelu <temilolakutelu@gmail.com>
2018-07-10 10:47:02 +0100

    apply page update

---
+++ react-app/src/pages/apply.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/header';
+import NavTop from '../components/navTop';
+import Apply from '../components/apply';
+import Footer from '../components/footer';
+
+export default () => (
+    <div>
+        <Header />
+        <NavTop />
+        <Apply />
+        <Footer />
+    </div>
+);
