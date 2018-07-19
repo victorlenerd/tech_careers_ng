cf0c1b138381fe1a02d89062ca4d27a4a32eb711
sapphire1996 <adepelumi1996@gmail.com>
2018-07-19 14:53:39 +0100

    changes

--- email-service/email.js
+++ email-service/email.js
@@ -19,7 +19,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.use(fileUpload());
 // Body Parser Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -30,74 +30,79 @@ app.use(bodyParser.json());
 
 
 app.post('/apply', (req, res) => {
-    let uploadFilename = req.files.resume.name;
-    let uploadData = req.files.resume.data;
-    let uploadEncoding = req.files.resume.encoding;
-    
+  let uploadFilename = req.files.resume.name;
+  let uploadData = req.files.resume.data;
+  let uploadEncoding = req.files.resume.encoding;
 
-    //mail text
-        mailText = (uploadFilename, uploadData, uploadEncoding) =>{
-            const output = `
+
+  //mail text
+  mailText = (uploadFilename, uploadData, uploadEncoding) => {
+    const output = `
+            <div style="display: block ">
             <p>Job Application at Techcareers NG</p>
-            <h3>Applicant's details</h3>
-            <ul>   
-              <li>Name: ${req.body.fullname}</li>
-              <li>Phone: ${req.body.phone}</li>
-              <li>Email: ${req.body.email}</li>
-            </ul>
+            <h3 style="margin: auto">Applicant's details</h3>
+            <div>   
+              <p>Name Of Applicant:<strong> ${req.body.fullname}</strong></p>
+              <br/>
+              <p>Phone:<strong>${req.body.phone}</strong></p>
+              <br/>
+              <p>Email:<strong> ${req.body.email}</strong></p>
+            </div>
             <div>
+            <h3>Cover Letter</h3>
                 <p>
                     ${req.body.coverletter}
                 </p>
             </div>
+            </div>
           `;
-        
-          // create reusable transporter object using the default SMTP transport
-          let transporter = nodemailer.createTransport({
-            host: 'mail.techbuzz.com.ng',
-            port: 465,
-            secure: true, 
-            auth: {
-                user: 'techcareers@techbuzz.com.ng', 
-                pass: 'w3bh4ck'  
-            },
-            tls:{
-              rejectUnauthorized:false
-            }
-          });
-        
-          // setup email data with unicode symbols
-          let mailOptions = {
-              from: '"Tech Careers NG" <techcareers@techbuzz.com.ng>', // sender address
-              to: req.body.to, // list of receivers
-              subject: req.body.jobTitle,
-              html: output,
-              attachments: [{
-                filename: uploadFilename,
-                content: uploadData,
-                encoding: uploadEncoding
-              }] 
-          };
-        
-          // send mail with defined transport object
-          transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                  return console.log("Email could not be sent", error);
-              }
-              console.log('Message sent: %s', info.messageId);   
-              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-              
-        
-            //   res.render('contact', {msg:'Email has been sent'});
-          });
 
-        //   res.render("submitted");
-        res.redirect('http://localhost:3000/apply?success=true');
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'mail.techbuzz.com.ng',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'techcareers@techbuzz.com.ng',
+        pass: 'w3bh4ck'
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
 
-        }
-        //end mail text
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: '"Tech Careers NG" <techcareers@techbuzz.com.ng>', // sender address
+      to: req.body.to, // list of receivers
+      subject: req.body.jobTitle,
+      html: output,
+      attachments: [{
+        filename: uploadFilename,
+        content: uploadData,
+        encoding: uploadEncoding
+      }]
+    };
 
-        mailText(uploadFilename, uploadData, uploadEncoding);
-  });
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log("Email could not be sent", error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+
+      //   res.render('contact', {msg:'Email has been sent'});
+    });
+
+    //   res.render("submitted");
+    res.redirect('http://localhost:3000/apply?success=true');
+
+  }
+  //end mail text
+
+  mailText(uploadFilename, uploadData, uploadEncoding);
+});
 
 app.listen(port, () => console.log(`Server started on....${port}`));
