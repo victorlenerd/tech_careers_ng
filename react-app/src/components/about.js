69b57615bd26b922ce0a0ed919f366d09c91b3c7
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-07-26 15:09:43 +0100

    edited the about page

--- react-app/src/components/about.js
+++ react-app/src/components/about.js
@@ -4,22 +4,26 @@ export default () => (
   <div>
     <div className="container my-5 pb-5 main">
       <div className="row">
-        <div className="col-md-10 offset-md-1 col-sm-12 about-div">
-          <h1 className="text-center">TechCareers NG</h1>
-          <p className="text-center about-text about-info">
+        <div className="col-md-10 offset-md-1 col-sm-12 about-div mt-5">
+          <h1 className="text-center about-theme">TechCareers NG</h1>
+          <p className=" about-text about-info">
             Tech is a popular industry to work in, partly because most tech jobs
             pay very well or the freedom it gives you to unleash your creativity
             and learn new things everyday. But where can you find jobs with
-            great tech companies? Well, <strong>TechCareers NG</strong> is one
-            place you can start. We put together the list of the best tech job
-            opportunities so you can quickly and easily find jobs in your city
-            to apply to. There is always a job form for every area of interests,
-            experience level and technology stack.
+            great tech companies? Well,
+            <strong className="about-theme font-italic">
+              {' '}
+              TechCareers NG{' '}
+            </strong>
+            is one place you can start. We put together the list of the best
+            tech job opportunities so you can quickly and easily find jobs in
+            your city to apply to. There is always a job form for every area of
+            interests, experience level and technology stack.
           </p>
         </div>
       </div>
 
-      <h1 className="text-center">The Team</h1>
+      <h1 className="text-center about-theme mt-5">The Team</h1>
 
       <div className="row">
         <div className="col-lg-3 col-md-6 pt-2 text-center mt-3">
@@ -32,12 +36,8 @@ export default () => (
             <div className="card-body">
               <h5 className="card-title">
                 Okwukwe Ewurum <br />
-                <span className="about-color font-weight-bold">Team Lead</span>
+                <span className="about-theme font-weight-bold">Team Lead</span>
               </h5>
-              {/* <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p> */}
               <a
                 href="https://www.linkedin.com/in/okwukwe-ewurum-1625a338/"
                 className="btn btn-linkedin">
@@ -60,10 +60,6 @@ export default () => (
                 <br />
                 <span className="about-color font-weight-bold">Developer</span>
               </h5>
-              {/* <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p> */}
               <a
                 href="https://www.linkedin.com/in/temilola-kutelu-714761a4/"
                 className="btn btn-linkedin">
@@ -108,10 +104,6 @@ export default () => (
                 <br />
                 <span className="about-color font-weight-bold">Developer</span>
               </h5>
-              {/* <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p> */}
               <a
                 href="https://www.linkedin.com/in/adepelumi/"
                 className="btn btn-linkedin">
